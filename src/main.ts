271d6a77d0f9d3f4dafd8eddfc82451984650302
Reinout Holtrup <reinout91@gmail.com>
2021-09-02 00:56:22 +0200

    working vue

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import "bootstrap/dist/css/bootstrap.css";
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "http://localhost:8000/";
 
 createApp(App).use(router).mount("#app");
