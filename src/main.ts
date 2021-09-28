f3dfad9673bcfd34094649a69234759d24c6467c
Reinout Holtrup <reinout91@gmail.com>
2021-09-28 22:21:38 +0200

    add ortuga

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { createApp } from 'vue';
 import axios from 'axios';
+import Oruga from '@oruga-ui/oruga-next';
+import '@oruga-ui/oruga-next/dist/oruga.css';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = 'http://localhost:8000/';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+app.use(router);
+app.use(Oruga);
+app.mount('#app');
